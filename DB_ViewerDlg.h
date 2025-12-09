f2eb666b52e1c59a3e5e188c7a05984616d75ddd
Logan Kang <hwan56270510@gmail.com>
2025-12-09 20:35:23 +0900

    Initial commit - DB_Viewer project

---
+++ DB_ViewerDlg.h
@@ -0,0 +1,34 @@
+﻿
+// DB_ViewerDlg.h: 헤더 파일
+//
+
+#pragma once
+
+
+// CDBViewerDlg 대화 상자
+class CDBViewerDlg : public CDialogEx
+{
+// 생성입니다.
+public:
+	CDBViewerDlg(CWnd* pParent = nullptr);	// 표준 생성자입니다.
+
+// 대화 상자 데이터입니다.
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_DB_VIEWER_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 지원입니다.
+
+
+// 구현입니다.
+protected:
+	HICON m_hIcon;
+
+	// 생성된 메시지 맵 함수
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+};
